d2f3f043a4486d8e61a2763ee86cd87f8fe28dfb
Yura Beseda <105202826+YuraBeseda@users.noreply.github.com>
2023-08-31 17:59:19 -0400

    Update settings.py to add allowed host for Vercel

--- blog/settings.py
+++ blog/settings.py
@@ -37,7 +37,7 @@ CSRF_COOKIE_SECURE = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "portfolio-marketplace.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "marketplace-irqmzgfl0-yurabeseda.vercel.app"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
